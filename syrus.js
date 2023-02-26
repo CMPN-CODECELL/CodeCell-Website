cd85bd7ba227a64232cb5c8ad9cbf9c48bed479a
maithilibrid <yashbrid03@gmail.com>
2023-02-26 15:00:33 +0530

    FinalFinalMerge3

--- syrus.js
+++ syrus.js
@@ -46,7 +46,6 @@ slider.addEventListener("mousemove", (e) => {
   const x = e.pageX - slider.offsetLeft;
   const walk = (x - startX) * 3; //scroll-fast
   slider.scrollLeft = scrollLeft - walk;
-  console.log(walk);
 });
 
 document.addEventListener("alpine:init", () => {
